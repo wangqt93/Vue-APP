77f8bedfc715ef0e251e176b538767940fedebaf
wangqt93 <969909031@qq.com>
2020-06-14 08:33:45 +0800

    初始化

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="app">
+   <p>hello</p>
+    <!-- 
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link> 
+     <router-view/>
+    </div>
+    -->
+  </div>
+</template>
+
---
+++ src/main.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+
+
+import FastClick from 'fastclick'
+FastClick.attach(document.body)
+
+
+import './assets/reset1.css'
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app1')
